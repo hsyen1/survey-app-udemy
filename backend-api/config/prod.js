b401dc0080a04901797a1d0e469add8e212017ad
Yen Han Sern <hansernyen@gmail.com>
2022-05-26 17:10:42 +0800

    refactored base URI to be dynamic base on environment

--- backend-api/config/prod.js
+++ backend-api/config/prod.js
@@ -3,4 +3,5 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  baseURI: "https://vast-island-80332.herokuapp.com",
 };
